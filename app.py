c63b2a0f10267f048efffe58fe84e7a104b417dd
Joe Shepard <shepard.joseph.m@gmail.com>
2022-07-20 17:07:50 -0700

    added completed statys to DB and functionality to flag and unflag items as completed or not

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import sys
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://joeshepard:testPass@localhost:5432/joe_db'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db) # this
+
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+#db.create_all() no linger using htis because we are changing the schema with migrations
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_complted_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -37,7 +56,7 @@ def create_todo():
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all()
+    return render_template('index.html', data=Todo.query.order_by('id').all()
     )
 
 if __name__ == '__main__':
